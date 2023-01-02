ee3351d56da0211ca708f23132af263d45f6dc7c
Tobias Düthorn <tobias@duethorn.cc>
2023-01-02 09:17:12 +0100

    added backlink on top

--- Install-PsExec.ps1
+++ Install-PsExec.ps1
@@ -1,4 +1,5 @@
-﻿$user = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+﻿# see https://github.com/kantholy/pstools
+$user = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 if (-Not $user.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)) {
     Write-Error "Need to be run as Administrator"
     exit 1
