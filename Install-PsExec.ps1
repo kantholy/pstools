5e472b2288a0a1c3e5205cf39eb2d76c8bf31855
Tobias Düthorn <tobias@duethorn.cc>
2023-01-02 08:59:02 +0100

    initial version

---
+++ Install-PsExec.ps1
@@ -0,0 +1,23 @@
+﻿#Requires -RunAsAdministrator
+
+$downloadLink = "https://download.sysinternals.com/files/PSTools.zip"
+$pstoolsZip = Join-Path -Path $env:TEMP -ChildPath "PSTools.zip"
+$targetFolder = $env:WINDIR
+
+# download
+Invoke-WebRequest -Uri $downloadLink -OutFile $pstoolsZip
+
+# extract
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+$zip = [System.IO.Compression.ZipFile]::OpenRead($pstoolsZip)
+$zip.Entries | Where-Object { $_.FullName -like "psexec*" } | ForEach-Object { 
+    $target = Join-Path -Path $targetFolder -ChildPath $_.Name
+    [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, $target, $true)
+}
+$zip.Dispose()
+
+# cleanup
+Remove-Item $pstoolsZip
+
+# accept EULA
+& reg ADD HKCU\Software\Sysinternals\PSexec /v EulaAccepted /t REG_DWORD /d 1 /f
